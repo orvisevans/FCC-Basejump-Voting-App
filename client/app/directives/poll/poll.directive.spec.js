8bddfb1619d968e2e966f3d070f4520585720ebd
Orvis Evans <b.orvis@gmail.com>
2015-08-15 00:07:43 -0700

    removed unused tests

--- client/app/directives/poll/poll.directive.spec.js
+++ client/app/directives/poll/poll.directive.spec.js
@@ -6,16 +6,17 @@ describe('Directive: poll', function () {
   beforeEach(module('workspaceApp'));
   beforeEach(module('app/directives/poll/poll.html'));
 
-  var element, scope;
+  // var element, scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<poll></poll>');
-    element = $compile(element)(scope);
-    scope.$apply();
-    expect(element.text()).toBe('this is the poll directive');
-  }));
+  // it('should make hidden element visible', inject(function ($compile) {
+  //   element = angular.element('<poll></poll>');
+  //   element = $compile(element)(scope);
+  //   scope.$apply();
+  //   expect(element.text()).toBe('this is the poll directive');
+  // }));
 });
